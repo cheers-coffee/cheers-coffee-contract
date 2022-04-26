430321f8f8ead85bf625a21ef2663c8e9c41203d
ImInnocent <ssje2006@naver.com>
2022-04-27 00:17:50 +0900

    version2 contract

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -6,36 +6,77 @@ import "./Ownable.sol";
 
 /// @title Donate
 /// @author ImInnocent
-/// @notice 외부에서 기부를 하면 내 지갑으로 돈을 쏴줍니다
+/// @notice 외부에서 기부를 하면 대상의 지갑으로 이더를 쏴줍니다
 contract Donate is Ownable {
-    uint public sum;
-    address payable private receiveAddress;
+    // 가입된 내용 목록
+    mapping (string => address payable) public registered;
+    // 유저별 받은 커피 합계
+    mapping (string => uint) public sum;
+    // 커피 가격
     uint public coffeePrice = 0.01 ether;
+    // 내 유저이름
+    string public ownerName = "goldfish";
 
+    /// @notice owner의 주소를 등록함. 
     constructor() {
-        receiveAddress = payable(_msgSender());
+        setRegistered(ownerName, payable(_msgSender()));
     }
 
-    function getReceiveAddress() view external onlyOwner returns (address) {
-        return receiveAddress;
+    /// @notice registered를 조회
+    /// @param _name 조회할 이름
+    function getRegistered(string memory _name) public view returns (address) {
+        return registered[_name];
+    }
+
+    /// @notice 내부적으로 사용되는 사용자 등록 로직
+    /// @param _name 등록자 이름
+    /// @param _addr 등록할 주소
+    function setRegistered(string memory _name, address payable _addr) private {
+        registered[_name] = _addr;
     }
 
     /// @notice 받은 커피의 개수를 보여줌. 소수점 없이 정수.
+    /// @param _user 조회할 유저의 이름
     /// @return 받은 커피의 개수.
-    function getReceivedCoffee() view external returns (uint) {
-        return sum / coffeePrice;
+    function getReceivedCoffee(string calldata _user) view external returns (uint) {
+        require(getRegistered(_user) != address(0));
+
+        return sum[_user] / coffeePrice;
     }
 
-    /// @notice 돈을 받을 주소를 바꿔주는 함수. (소유자 전용)
+    /// @notice 커피 가격 바꾸기 (소유자 전용)
+    /// @param _coffeePrice 바꿀 가격 (0보다 커야 함)
+    function changeCoffeePrice(uint _coffeePrice) external onlyOwner {
+        require(_coffeePrice > 0);
+
+        coffeePrice = _coffeePrice;
+    }
+
+    /// @notice 소유자의 주소를 바꿔주는 함수. (소유자 전용)
     /// @param _addr 바꿀 새 주소
     function changeReceiveAddress(address _addr) external onlyOwner {
-        require(receiveAddress != payable(_addr));
-        receiveAddress = payable(_addr);
+        require(getRegistered(ownerName) != payable(_addr));
+
+        setRegistered(ownerName, payable(_addr));
     }
 
-    /// @notice 돈을 받으면 컨트랙트의 주인 주소로 보내주는 함수
-    function donate() payable external {
-        receiveAddress.transfer(msg.value);
-        sum = sum + msg.value;
+    /// @notice 기부를 받으면 컨트랙트의 주인 주소로 보내주는 함수
+    /// @param _user 기부를 받을 유저 이름
+    function donate(string calldata _user) payable external {
+        // 등록된 주소가 있어야 기부 가능
+        require(registered[_user] != address(0));
+
+        registered[_user].transfer(msg.value);
+        sum[_user] = sum[_user] + msg.value;
+    }
+
+    /// @notice 새로운 사람을 등록하는 함수
+    /// @param _user 유저 이름
+    /// @param _addr 등록할 주소
+    function register(string calldata _user, address _addr) external {
+        // 등록된 주소가 없어야 등록 가능
+        require(getRegistered(_user) == address(0));
+
+        setRegistered(_user, payable(_addr));
     }
 }
--- test/Donate.js
+++ test/Donate.js
@@ -1,31 +1,77 @@
 const Donate = artifacts.require("Donate");
 const utils = require("./helpers/utils");
 const time = require("./helpers/time");
+const { assert, should } = require("chai");
 const expect = require("chai").expect;
 
 contract("Donate", (accounts) => {
   let contract;
-  const [userA, userB] = accounts;
+  const [userA, userB, userC] = accounts;
 
   beforeEach(async () => {
     contract = await Donate.new();
   });
 
-  it("should receiveAddress is same as userA", async () => {
-    const receiveAddress = await contract.getReceiveAddress({ from: userA });
-    expect(receiveAddress.to.equal(userA));
+  context("초기값 테스트", async () => {
+    it("'ownerName'이 'goldfish'인가", async () => {
+      const ownerName = await contract.ownerName({ from: userA });
+      assert.equal(ownerName, "goldfish");
+    });
+
+    it("'goldfish'가 소유자 주소로 등록되어있는가", async () => {
+      const ownerName = await contract.ownerName({ from: userA });
+      const registered = await contract.getRegistered(ownerName, { from: userA });
+      assert.equal(registered, userA);
+    })
+
+    it("'coffeePrice'가 0.01 ether인가", async () => {
+      const eth = web3.utils.toWei("0.01", 'ether');
+      const coffeePrice = await contract.coffeePrice({ from: userA });
+      assert.equal(coffeePrice, eth);
+    });
+  })
+
+  context("'changeCoffeePrice' 테스트", async () => {
+    it("'coffeePrice'가 변경되었는가", async () => {
+      await contract.changeCoffeePrice(1, { from: userA });
+      const coffeePrice = await contract.coffeePrice({ from: userA });
+      assert.equal(coffeePrice, 1);
+    });
+
+    // it("should owner can access changeCoffeePrice", async () => {
+    //   await contract.changeCoffeePrice(1, { from: userB }).should.be.rejected();
+    // });
   });
 
-  it("should only owner can access to getReceiveAddress", async () => {
-    assert.throws(await contract.getReceiveAddress({ from: userB }), "should throw");
+  context("'changeReceiveAddress' 테스트", async () => {
+    it("등록된 주소가 실제로 바뀌는가", async () => {
+      await contract.changeReceiveAddress(userC, { from: userA });
+      const ownerName = await contract.ownerName({ from: userA });
+      const registered = await contract.getRegistered(ownerName, { from: userA });
+      assert.equal(registered, userC);
+    });
+
+    it("주소 변경 이후 실제 기부 시에 돈이 정상 전송되는가", async () => {
+      await contract.changeReceiveAddress(userC, { from: userA });
+
+      const ownerName = await contract.ownerName({ from: userA });
+      const beforeBalance = await web3.eth.getBalance(userC);
+      const eth = web3.utils.toWei("0.01", 'ether');
+
+      await contract.donate(ownerName, { from: userB, value: eth });
+
+      const afterBalance = await web3.eth.getBalance(userC);
+      
+      assert.equal(afterBalance - beforeBalance, eth);
+    });
   });
 
-  it("should sum is sum of donations", async () => {
-    const eth = web3.utils.toWei("2", 'ether');
-    await contract.donate({ from: userB, value: eth });
-    const sum = await contract.sum({ from: userB });
-    expect(sum.toString()).to.equal(eth);
-  });
+  // it("should sum is sum of donations", async () => {
+  //   const eth = web3.utils.toWei("2", 'ether');
+  //   await contract.donate({ from: userB, value: eth });
+  //   const sum = await contract.sum({ from: userB });
+  //   assert.equal(sum.toString(), eth);
+  // });
 });
 
 
