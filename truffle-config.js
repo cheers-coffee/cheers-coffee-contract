3889eef6cfc7851e86659aa0dda635a78bb6bdbd
ImInnocent <ssje2006@naver.com>
2022-04-28 17:43:15 +0900

    Commit config

--- truffle-config.js
+++ truffle-config.js
@@ -3,10 +3,14 @@ const Web3 = require("web3");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const web3 = new Web3();
 
-const getEnv = env => {
+const getEnv = (env, _default) => {
   const value = process.env[env];
 
   if (typeof value === 'undefined') {
+    if (_default !== undefined) {
+      return _default;
+    }
+
     throw new Error(`${env} has not been set.`);
   }
 
@@ -15,7 +19,12 @@ const getEnv = env => {
 
 const mnemonic = getEnv('ETH_WALLET_MNEMONIC');
 const liveNetwork = getEnv('ETH_LIVE_NETWORK');
-const liveNetworkId = parseInt(getEnv('ETH_LIVE_NETWORK_ID'));
+const liveNetworkId = parseInt(getEnv('ETH_LIVE_NETWORK_ID', 1));
+const liveGasPrice = parseInt(getEnv('ETH_LIVE_GAS_PRICE', web3.utils.toWei('40', 'gwei')));
+
+const ropstenNetwork = getEnv('ETH_ROPSTEN_NETWORK');
+const ropstenNetworkId = parseInt(getEnv('ETH_ROPSTEN_NETWORK_ID', 3));
+const ropstenGasPrice = parseInt(getEnv('ETH_ROPSTEN_GAS_PRICE', web3.utils.toWei('10', 'gwei')));
 
 module.exports = {
   // Object with configuration for each network
@@ -27,15 +36,26 @@ module.exports = {
       network_id: 5777,
       gas: 6721975
     },
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, ropstenNetwork),
+      network_id: ropstenNetworkId,
+      gasPrice: ropstenGasPrice,
+    },
     live: {
       provider: () => new HDWalletProvider(mnemonic, liveNetwork),
       network_id: liveNetworkId,
-      gasPrice: web3.utils.toWei('80', 'gwei')
+      gasPrice: liveGasPrice,
     },
   },
   compilers: {
     solc: {
       version: "^0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+      },
     },
   },
 };
